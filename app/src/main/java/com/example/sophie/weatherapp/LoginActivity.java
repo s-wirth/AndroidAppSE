1f0d4f4aa183a123db675d72bc7a3d0274b082ce
Sophie <a.sophiewirth@gmail.com>
2015-05-18 16:48:30 +0200

    Added Range Seekbar to Route Planning Screen

--- app/src/main/java/com/example/sophie/weatherapp/LoginActivity.java
+++ app/src/main/java/com/example/sophie/weatherapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -59,33 +60,33 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
-
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+//
+//        // Set up the login form.
+//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+//        populateAutoComplete();
+//
+//        mPasswordView = (EditText) findViewById(R.id.password);
+//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+//                    attemptLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//
+//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
+//
+//        mLoginFormView = findViewById(R.id.login_form);
+//        mProgressView = findViewById(R.id.login_progress);
     }
 
     @Override
@@ -116,68 +117,71 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         getLoaderManager().initLoader(0, null, this);
     }
 
+    public void switchActivity(View v){
+        startActivity(new Intent(LoginActivity.this, RoutePlanning.class));
+    }
 
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    public void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
-        }
-    }
-
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
+//    public void attemptLogin() {
+//        if (mAuthTask != null) {
+//            return;
+//        }
+//
+//        // Reset errors.
+//        mEmailView.setError(null);
+//        mPasswordView.setError(null);
+//
+//        // Store values at the time of the login attempt.
+//        String email = mEmailView.getText().toString();
+//        String password = mPasswordView.getText().toString();
+//
+//        boolean cancel = false;
+//        View focusView = null;
+//
+//        // Check for a valid password, if the user entered one.
+//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+//            mPasswordView.setError(getString(R.string.error_invalid_password));
+//            focusView = mPasswordView;
+//            cancel = true;
+//        }
+//
+//        // Check for a valid email address.
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailView.setError(getString(R.string.error_field_required));
+//            focusView = mEmailView;
+//            cancel = true;
+//        } else if (!isEmailValid(email)) {
+//            mEmailView.setError(getString(R.string.error_invalid_email));
+//            focusView = mEmailView;
+//            cancel = true;
+//        }
+//
+//        if (cancel) {
+//            // There was an error; don't attempt login and focus the first
+//            // form field with an error.
+//            focusView.requestFocus();
+//        } else {
+//            // Show a progress spinner, and kick off a background task to
+//            // perform the user login attempt.
+//            showProgress(true);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
+//        }
+//    }
+//
+//    private boolean isEmailValid(String email) {
+//        //TODO: Replace this with your own logic
+//        return email.contains("@");
+//    }
+//
+//    private boolean isPasswordValid(String password) {
+//        //TODO: Replace this with your own logic
+//        return password.length() > 4;
+//    }
 
     /**
      * Shows the progress UI and hides the login form.
@@ -325,5 +329,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
         }
     }
+
+
 }
 
--- app/src/main/java/com/example/sophie/weatherapp/RoutePlanning.java
+++ app/src/main/java/com/example/sophie/weatherapp/RoutePlanning.java
@@ -1,17 +1,102 @@
 package com.example.sophie.weatherapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class RoutePlanning extends ActionBarActivity {
+public class RoutePlanning extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_planning);
+        Spinner();
+        RangeBar();
+
+
+    }
+
+    public void Spinner(){
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        Spinner spinner_dif = (Spinner) findViewById(R.id.spinner_dif);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.location_option, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        spinner2.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+        ArrayAdapter<CharSequence> adapter_dif = ArrayAdapter.createFromResource(
+                this, R.array.difficulty, android.R.layout.simple_spinner_item);
+        adapter_dif.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_dif.setAdapter(adapter_dif);
+        spinner_dif.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+        Button myButton =(Button)findViewById(R.id.button1);
+        myButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                Spinner sp = (Spinner) findViewById(R.id.spinner);
+                String spinnerString = null;
+                spinnerString = sp.getSelectedItem().toString();
+                int nPos = sp.getSelectedItemPosition();
+
+
+                Toast.makeText(getApplicationContext(), "getSelectedItem=" + spinnerString,
+                        Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "getSelectedItemPosition=" + nPos,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void RangeBar(){
+        // create RangeSeekBar as Integer range between 0 and 50
+        RangeSeekBar<Integer> seekBar = new RangeSeekBar<Integer>(0, 50, this.getApplicationContext());
+        seekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
+            int originalMin = -1;
+            int originalMax = -1;
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
+                // handle changed range values
+                Log.i("RoutePlanning", "User selected new range values: MIN=" + minValue + ", MAX=" + maxValue);
+
+
+
+                if((originalMin != minValue) && (minValue != 0)) {
+                    originalMin = minValue;
+                    Toast.makeText(getApplicationContext(), "Not less than " + minValue + "km",
+                            Toast.LENGTH_LONG).show();
+
+
+                }
+
+                if((originalMax != maxValue) && (maxValue != 50)) {
+                    originalMax = maxValue;
+                    Toast.makeText(getApplicationContext(), "Not more than " + maxValue + "km",
+                            Toast.LENGTH_LONG).show();
+
+
+                }
+
+            }
+        });
+
+        // add RangeSeekBar to pre-defined layout
+        ViewGroup layout = (ViewGroup) findViewById(R.id.rangeBarLayout);
+        layout.addView(seekBar);
     }
 
     @Override
@@ -35,4 +120,6 @@ public class RoutePlanning extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
+
